be9fa0c08968024123b245f8e38590de465f89e8
gabor santa <gabor santa>
2016-12-01 11:21:48 -0500

    validation

--- server.js
+++ server.js
@@ -46,38 +46,12 @@ app.post('/process_post', urlencodedParser, function (req, res) {
 				console.log('All good');
 			}
 
-	 		});
-
+	 });
 
    		res.end("Message Sent");
 });
 
 
-/* app.post("/", function(req, res){
-
-    console.log("a user has request /the-form via POST");
-
-		    //form processing code goes here....
-		//var accountSid = 'ACd8a0efd010e09e09ce506fe1d4612e11';
-		var accountSid = 'ACd8a0efd010e09e09ce506fe1d4612e11';
-		var authToken = '49bb0dcc9d997acf2e30c830b2859e0b';
-
-
-			//require the Twilio module and create a REST client
-		var client = require('twilio')(accountSid, authToken);
-
-
-		client.messages.create({
-		    to: "+16463316126",
-		    from: "+19179243374",
-		    body: "This message is from Gabor",
-		}, function(err, message) {
-		    console.log(message.sid);
-		});
-    //send message to user
-    //response.send("okay, we'll process that right away.")
-});
-*/
 app.listen(port, function() {
 	console.log('Our app is running on http://localhost:' + port);
 });
